c4d32bd2be48ea521c241416c89759a8dac3b2b9
Krasimir Kostadinov <krasimirkostadinov9@gmail.com>
2023-03-10 16:48:52 +0200

    install webpack and config

--- src/index.js
+++ src/index.js
@@ -1,4 +1,57 @@
-console.log(__dirname);
+import { html, render } from 'lit-html';
+
+
+const template = (ctx) => html`
+<div>
+    <button id="btn" @click=${ctx.myShow}>show header</button>
+    <h1 id="header">Im header</h1>
+</div>
+    `;
+
+
+
+class AppRoot extends HTMLElement {
+    constructor() {
+        super();
+        this.name = 'Krasimir';
+        this.fullName = `${this.name} Kostadinov`;
+        const root = this.attachShadow({ mode: 'closed' });
+        this.update = () => {
+            render(template(this), root, { eventContext: true });
+        }
+
+        this.myShow = (event) => {
+            const divEl = event.target.parentElement;
+            const header = divEl.children[1];
+            if (event.target.textContent === 'show header') {
+                event.target.textContent = 'hide header';
+                header.textContent = this.fullName;
+                header.style.display = 'block';
+                this.update();
+            } else {
+                event.target.textContent = 'show header';
+                header.textContent = '';
+                header.style.display = 'none';
+                this.update();
+            }
+        }
+
+        this.update();
+    }
+
+    connectedCallback() {
+
+    }
+
+
+
+}
+
+customElements.define('app-root', AppRoot);
+
+
+
+
 
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,22 @@
 
-
 module.exports = {
+
+    mode: 'none',
+
     entry: './src/index.js',
     output: {
-        
-    }
+        path: __dirname + '/dist',
+        filename: 'app.bundle.js'
+    },
+
+    devServer: {
+        static: {
+            directory: __dirname,
+        },
+        compress: true,
+        port: 9000,
+    },
+
+
+
 }
