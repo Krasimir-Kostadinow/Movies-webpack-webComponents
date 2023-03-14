b95763974589ebd2534a111ecf45290db3e30708
Krasimir Kostadinov <krasimirkostadinov9@gmail.com>
2023-03-14 15:18:16 +0200

    add raw-loader and bable-loader

--- src/index.js
+++ src/index.js
@@ -1,21 +1,34 @@
-import { html, render } from 'lit-html';
-
+const { html, render } = require('lit-html');
+const axios = require('axios').default;
+const text = require('../src/commands.txt');
+const appStyle = require('../src/app.css');
 
 const template = (ctx) => html`
+<style>${appStyle}</style>
 <div>
     <button id="btn" @click=${ctx.myShow}>show header</button>
-    <h1 id="header">Im header</h1>
+    <h1 id="header">header</h1>
+    <button @click=${ctx.krasi}>krasi console</button>
+    <h2>${ctx.contentText}</h2>
 </div>
     `;
 
 
 
 class AppRoot extends HTMLElement {
+
+    contentText = text;
+
+    krasi = () => {
+        console.log('krasi');
+        console.log(this);
+    };
+
     constructor() {
         super();
         this.name = 'Krasimir';
-        this.fullName = `${this.name} Kostadinov`;
-        const root = this.attachShadow({ mode: 'closed' });
+        this.fullName = `${this.name} Kostadinov and heks`;
+        const root = this.attachShadow({ mode: 'open' });
         this.update = () => {
             render(template(this), root, { eventContext: true });
         }
@@ -40,7 +53,10 @@ class AppRoot extends HTMLElement {
     }
 
     connectedCallback() {
-
+        console.log(123);
+        axios.get('https://jsonplaceholder.typicode.com/users').then((users) => {
+            console.log(users);
+        });
     }
 
 
@@ -52,8 +68,18 @@ customElements.define('app-root', AppRoot);
 
 
 
+// const root = document.getElementById('root');
 
 
 
 
+// class App {
+//     constructor(name) {
+//         this.base = "My name is";
+//         this.content = `${this.base} ${name}`;
+//     }
+// }
 
+// let data = new App('Krasimir');
+// console.log(data);
+// root.textContent = data.content;
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,50 @@
 
 module.exports = {
 
-    mode: 'none',
+    mode: 'production',
 
     entry: './src/index.js',
     output: {
         path: __dirname + '/dist',
         filename: 'app.bundle.js'
+        // sourceMapFilename: "app.bundle.js.map"
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                        plugins: ['@babel/plugin-proposal-class-properties']
+                    }
+                }
+            },
+            {
+                test: /\.(txt$|css)/i,
+                use: [
+                  {
+                    loader: 'raw-loader',
+                    options: {
+                      esModule: false,
+                    },
+                  },
+                ],
+              },   
+        ]
     },
 
     devServer: {
-        static: {
-            directory: __dirname,
-        },
+        contentBase: __dirname,
         compress: true,
         port: 9000,
     },
+
+    // devtool: "source-map",
+
 
 
 
