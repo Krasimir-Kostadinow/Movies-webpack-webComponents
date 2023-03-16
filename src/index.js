98d2b8777f56ae783e083798f56e280572c5a86f
Krasimir Kostadinov <krasimirkostadinov9@gmail.com>
2023-03-16 15:37:36 +0200

    exercise webpack

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+
 const { html, render } = require('lit-html');
 const axios = require('axios').default;
-const text = require('../src/commands.txt');
-const appStyle = require('../src/app.css');
+
+import text from '../src/commands.txt';
+import appStyle from '../src/app.css';
 import * as utils from './utils';
 
 const template = (ctx) => html`
@@ -9,10 +11,11 @@ const template = (ctx) => html`
 <div>
     <button id="btn" @click=${ctx.myShow}>show header</button>
     <h1 id="header">header</h1>
-    <input id="content" type="text"/> 
+    <input id="content" type="text" />
     <br><br><br>
     <button @click=${ctx.krasi}>show name</button>
-    <h2>${ctx.contentText}</h2>
+    <h2>${ctx.textContent}</h2>
+
 </div>
     `;
 
@@ -20,7 +23,7 @@ const template = (ctx) => html`
 
 class AppRoot extends HTMLElement {
 
-    contentText = text;
+    textContent = text;
 
     krasi = (event) => {
         const divEl = event.target.parentElement;
@@ -29,12 +32,13 @@ class AppRoot extends HTMLElement {
         let h2El = divEl.children[7];
         h2El.textContent = result;
 
+
     };
 
     constructor() {
         super();
         this.name = 'Krasimir';
-        this.fullName = `${this.name} Kostadinov and heks`;
+        this.fullName = `${this.name} Kostadinov`;
         const root = this.attachShadow({ mode: 'open' });
         this.update = () => {
             render(template(this), root, { eventContext: true });
@@ -81,9 +85,6 @@ customElements.define('app-root', AppRoot);
 
 // const root = document.getElementById('root');
 
-
-
-
 // class App {
 //     constructor(name) {
 //         this.base = "My name is";
@@ -94,3 +95,45 @@ customElements.define('app-root', AppRoot);
 // let data = new App('Krasimir');
 // console.log(data);
 // root.textContent = data.content;
+
+// const template = (ctx) => html`
+
+// <div>
+//     <h1>Hello Webpack</h1>
+//     <p>${ctx.krasi('Alex')}</p>
+// </div>
+//     `;
+
+
+
+// class AppRoot extends HTMLElement {
+
+//     krasi = (name) => {
+//         let result = utils.myName(name);
+//         return result;
+//     };
+
+//     constructor() {
+//         super();
+//         this.name = 'Krasimir';
+//         this.fullName = `${this.name} Kostadinov`;
+//         const root = this.attachShadow({ mode: 'open' });
+//         this.update = () => {
+//             render(template(this), root, { eventContext: true });
+//         }
+
+//         this.update();
+//     }
+
+//     connectedCallback() {
+//         axios.get('https://jsonplaceholder.typicode.com/users').then((users) => {
+//             console.log(users);
+//         });
+
+//     }
+
+
+
+// }
+
+// customElements.define('app-root', AppRoot);
--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,39 @@
+const path = require('path');
 
 module.exports = {
+  mode: 'production',
+  devtool: 'source-map',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
 
-    mode: 'production',
+  devServer: {
+    static: './dist',
+  },
 
-    devtool: 'sourcemap',
-
-    entry: './src/app.js',
-    output: {
-        path: __dirname + '/dist',
-        filename: 'app.bundle.js'
-        // sourceMapFilename: "app.bundle.js.map"
-    },
-
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env'],
-                        plugins: ['@babel/plugin-proposal-class-properties']
-                    }
-                }
-            },
-            {
-                test: /\.(txt$|css)/i,
-                use: [
-                  {
-                    loader: 'raw-loader',
-                    options: {
-                      esModule: false,
-                    },
-                  },
-                ],
-              },   
-        ]
-    },
-
-    devServer: {
-        contentBase: __dirname,
-        compress: true,
-        port: 9000,
-    },
-
-    // devtool: "source-map",
-
-
-
-
-}
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ],
+            plugins: ['@babel/plugin-proposal-class-properties']
+          }
+        }
+      },
+      {
+        test: /\.(txt|css)$/i,
+        use: 'raw-loader',
+      },
+ 
+    ]
+  },
+  
+};
